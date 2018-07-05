573b12c73389253f5f9bac6f86ce4db46e756016
Wisnu Anggoro <wisnu@anggoro.net>
2018-07-05 17:14:30 +0700

    Adding Login Box
    Add login box but no login functionality

---
+++ Toserba/Toserba/Secrets/FirebaseSensitiveData.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseSensitiveData.swift
+//  Toserba
+//
+//  Created by MTMAC18 on 05/07/18.
+//  Copyright © 2018 Wisnu Anggoro. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseSensitiveData {
+    // Initialize Singleton
+    private init(){}
+    static let shared = FirebaseSensitiveData()
+    
+    // Sensitive data
+    let apiKey = ""
+}
