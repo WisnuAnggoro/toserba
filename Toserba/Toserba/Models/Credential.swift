779ce71fb9521bc30f80b1dc57189477a7536033
Wisnu Anggoro <wisnu@anggoro.net>
2018-07-06 17:14:33 +0700

    Adding login capability

---
+++ Toserba/Toserba/Models/Credential.swift
@@ -0,0 +1,20 @@
+//
+//  Credential.swift
+//  Toserba
+//
+//  Created by MTMAC18 on 06/07/18.
+//  Copyright © 2018 Wisnu Anggoro. All rights reserved.
+//
+
+import Foundation
+
+class Credential {
+    var kind = ""
+    var localId = ""
+    var email = ""
+    var displayName = ""
+    var idToken = ""
+    var registered = false
+    var refreshToken = ""
+    var expiresIn = ""
+}
