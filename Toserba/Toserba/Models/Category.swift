fdfdab8809a9de1f449b437cd84b6af530c8485e
Wisnu Anggoro <wisnu@anggoro.net>
2018-07-10 16:40:47 +0700

    Completing Login Feature

---
+++ Toserba/Toserba/Models/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Toserba
+//
+//  Created by Wisnu Anggoro on 10/07/18.
+//  Copyright © 2018 Wisnu Anggoro. All rights reserved.
+//
+
+import Foundation
+
+class Category {
+    var id = 0
+    var name = ""
+}
